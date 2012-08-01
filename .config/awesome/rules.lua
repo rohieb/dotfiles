2dd8e9475e2e53bb2552b8503f6350d4ba04008f
Roland Hieber <rohieb@rohieb.name>
2012-08-01 16:50:25 +0200

    Awesome: JOSM rules

--- .config/awesome/rules.lua
+++ .config/awesome/rules.lua
@@ -19,10 +19,14 @@ awful.rules.rules = {
     { rule = { class = "Knetwalk" }, properties = { floating = true } },
     { rule = { name = "Terminator Preferences" }, properties = { floating = true } },
     { rule = { class = "Epiphany-browser" }, properties = { floating = false } },
+    { rule = { class = "org-openstreetmap-josm-Main" }, properties = { floating = false } },
 
     -- map specific windows to specific tags
     { rule = { class = "Gkrellm" },
       properties = { tag = tags[1][1] } },
+
+    { rule = { class = "org-openstreetmap-josm-Main" },
+      properties = { tag = tags[1][2] } },
 
     { rule = { class = "WikidPad" },
       properties = { tag = tags[1][3] } },
