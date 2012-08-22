e39ae52a877d228b6fcf6eee71c4b42bad6bae3e
Roland Hieber <rohieb@rohieb.name>
2012-08-22 23:02:00 +0200

    Awesome: resolve shortcut conflict

--- .config/awesome/bindings.lua
+++ .config/awesome/bindings.lua
@@ -48,8 +48,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+    awful.key({ modkey, "Shift"   }, "j",     function () awful.tag.incncol( 1)         end),
+    awful.key({ modkey, "Shift"   }, "k",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
