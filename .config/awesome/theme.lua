fc42405710df2a45b9f17ca2148a25336a01a684
Roland Hieber <rohieb@rohieb.name>
2013-02-07 02:27:20 +0100

    Awesome: better wallpaper rotation that also remembers the wallpaper when restarting Awesome

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -2,7 +2,7 @@
 dofile("/usr/share/awesome/themes/default/theme.lua")
 
 -- set the wallpaper
-theme.wallpaper_cmd = { "rotate-wallpaper" }
+theme.wallpaper_cmd = { "set-wallpaper" }
 
 return theme
 -- vim: set ts=2 sw=2 et:
