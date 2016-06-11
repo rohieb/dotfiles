7b96efff37367b10080d38d8188d0868c02343c7
Roland Hieber <rohieb@rohieb.name>
2016-06-11 06:22:59 +0200

    awesome: remove unused debugging code

--- .config/awesome/debug.lua
+++ .config/awesome/debug.lua
@@ -9,27 +9,4 @@ function dbg(vars)
     naughty.notify({ text = text, timeout = 0 })
 end
 
---
--- print a table recursively
--- from: http://lua-users.org/wiki/TableSerialization
---
-function print_r (t, indent, done)
-  done = done or {}
-  indent = indent or ''
-  local nextIndent -- Storage for next indentation value
-  for key, value in pairs (t) do
-    if type (value) == "table" and not done [value] then
-      nextIndent = nextIndent or
-          (indent .. string.rep(' ',string.len(tostring (key))+2))
-          -- Shortcut conditional allocation
-      done [value] = true
-      print (indent .. "[" .. tostring (key) .. "] => Table {");
-      print  (nextIndent .. "{");
-      print_r (value, nextIndent .. string.rep(' ',2), done)
-      print  (nextIndent .. "}");
-    else
-      print  (indent .. "[" .. tostring (key) .. "] => " .. tostring (value).."")
-    end
-  end
-end
 -- vim: set tw=80 ts=2 sw=2 et:
