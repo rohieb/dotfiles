6622998bee364291479333562dacabdd64aa5ff4
Roland Hieber <rohieb@rohieb.name>
2016-11-22 14:27:50 +0100

    udiskie: don't automount new partitions

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -10,7 +10,7 @@ procs = {
   { "pasystray", },
   { "setkeymap", },
   { "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1", },
-  { "udiskie", "udiskie --automount --notify --smart-tray" },
+  { "udiskie", "udiskie --no-automount --notify --smart-tray" },
   { "hamster-time-tracker", },
 --  { "ibus-daemon", "ibus-ademon --xim" },
 }
