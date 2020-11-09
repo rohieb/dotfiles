79cc64340ca4bfca70029dc893c1777d33eb366c
Roland Hieber <rohieb@rohieb.name>
2020-11-09 18:31:41 +0100

    ff2mpv: add mozilla native client

---
+++ opt/ff2mpv.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Firefox "Open in MPV" plugin
+# forked and adapted from https://github.com/woodruffw/ff2mpv/blob/master/ff2mpv.py
+# for https://addons.mozilla.org/en-US/firefox/addon/ff2mpv/
+
+import json
+import platform
+import struct
+import sys
+import subprocess
+
+
+def main():
+    message = get_message()
+    url = message.get("url")
+
+    args = ["mpve", url]
+
+    kwargs = {}
+    # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#Closing_the_native_app
+    if platform.system() == "Windows":
+        kwargs["creationflags"] = subprocess.CREATE_BREAKAWAY_FROM_JOB
+
+    subprocess.Popen(args, **kwargs)
+
+    # Need to respond something to avoid "Error: An unexpected error occurred"
+    # in Browser Console.
+    send_message("ok")
+
+
+# https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Native_messaging#App_side
+def get_message():
+    raw_length = sys.stdin.buffer.read(4)
+    if not raw_length:
+        return {}
+    length = struct.unpack("@I", raw_length)[0]
+    message = sys.stdin.buffer.read(length).decode("utf-8")
+    return json.loads(message)
+
+
+def send_message(message):
+    content = json.dumps(message).encode("utf-8")
+    length = struct.pack("@I", len(content))
+    sys.stdout.buffer.write(length)
+    sys.stdout.buffer.write(content)
+    sys.stdout.buffer.flush()
+
+
+if __name__ == "__main__":
+    main()
