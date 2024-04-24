rohieb/dotfiles

commit 49a67607a485a5b765f3302646be86723f06fcd5

bash: PS1: add bitbake environment marker

.bashrc
@@ -83,6 +83,12 @@ __ps1_screen_window_title() {
 	fi
 }
 
+__ps1_bitbake() {
+	if [ -n "$BBPATH" ]; then
+		printf " (BB)"
+	fi
+}
+
 PS1_WITH_HOSTNAME=
 if [ -n "$SSH_CONNECTION" ]; then
 	PS1_WITH_HOSTNAME=1
@@ -90,6 +96,7 @@ fi
 
 PS1="\[${__BLUE}\]\t\[${__RESET}\]"
 PS1="${PS1}${debian_chroot:+ ($debian_chroot)}"
+PS1="${PS1}\[${__YELLOW}\]\$(__ps1_bitbake)\[${__RESET}\]"
 PS1="${PS1}\[${__RED}\]\$(__ps1_status)\[${__RESET}\]"
 if [ -n "$PS1_WITH_HOSTNAME" ]; then PS1="${PS1}\[${__RED}\] \u@\h\[${__RESET}\]"; fi
 PS1="${PS1} \[${__GREEN}\]\$(__ps1_shortpwd)\[${__RESET}\]"

commit 3ae6613ac172e20e009b644993c09afcefcc7fee

bashrc: fix showing return value of last command
Calling __ps1_bitbake overwrites $?. Sort it after __ps1_status.

Fixes: 49a67607a485a5b765f3 (2022-11-27, "bash: PS1: add bitbake environment marker")

.bashrc
@@ -159,8 +159,8 @@ TTY="${TTY//\/dev\//}"
 
 PS1="\[${__BLUE}\]\t\[${__RESET}\]"
 PS1="${PS1}${debian_chroot:+ ($debian_chroot)}"
-PS1="${PS1}\[${__YELLOW}\]\$(__ps1_bitbake)\[${__RESET}\]"
 PS1="${PS1}\[${__RED}\]\$(__ps1_status)\[${__RESET}\] ${TTY}"
+PS1="${PS1}\[${__YELLOW}\]\$(__ps1_bitbake)\[${__RESET}\]"
 if [ -n "$PS1_WITH_HOSTNAME" ]; then PS1="${PS1}\[${__RED}\] \u@\h\[${__RESET}\]"; fi
 PS1="${PS1} \[${__GREEN}\]\$(__ps1_shortpwd)\[${__RESET}\]"
 PS1="${PS1}\[${__YELLOW}\]\$(__ps1_env_var \${MACHINE})\[${__RESET}\]"