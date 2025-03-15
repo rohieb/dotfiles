4603851a023e0e25b443553d3540f552906a488d
Roland Hieber <rohieb@rohieb.name>
2025-03-15 23:21:59 +0100

    ff2mpv: sync from upstream
    Merge the following commits from <https://github.com/woodruffw/ff2mpv/>:
    
      | commit 51a655952bd7788d10064362c96a2f9e2c69c4c1
      | Author: guest271314 <guest271314@gmail.com>
      | Date:   So 2024-07-28 11:35:15
      |
      |     Eliminate space counted as message length (#126)
      |
      |     Co-authored-by: William Woodruff <william@yossarian.net>
      |
      | commit f57d39bba6b826d5ebc4b452067e7e803bad1118
      | Author: William Woodruff <william@yossarian.net>
      | Date:   Sa 2024-05-04 17:17:46
      |
      |     native clients: flip brew paths
      |
      |     See #124.
      |
      | commit 8b828f97a7da09c0555ee6c8cb0a95c54c95c728
      | Author: Claudio Freitas <claudiofreitas3@gmail.com>
      | Date:   Fr 2023-12-01 06:47:07
      |
      |     Include the prefix /opt/homebrew/bin in the PATH for installations that were performed for arch arm64 (#111)
      |
      | commit 7f0eb17f70db23e1bb4d63ec1fde7f4178919c72
      | Author: Eduardo D Sanchez <eduardo.daniel.s.m@gmail.com>
      | Date:   So 2023-11-19 10:46:26
      |
      |     Feat/mpv profiles (#108)
      |
      |     * feat: Add options menu for extension
      |
      |     * feat: Add options.html to manifest and storage permissions
      |
      |     * fix: delete option without id error
      |
      |     * feat: Update native client ff2mpv.py
      |
      |     * feat: Update ff2mpv.js to allow multiple context menus
      |
      |     * refactor: Remove logs
      |
      |     * refactor: Add new line at end of file
      |
      |     * feat: Update ruby client to support options
      |
      |     * refactor: Reduce unnecessary function calls
      |
      |     * refactor: Trim lines and prevent sending empty arguments
      |
      |     * fix: Prevent access to dead objects
      |
      |     * refactor: Remove uuid wrapper function
      |
      |     * refactor: Improve css handling for light and dark theme
      |
      |     * feat: Add warning message on top of options page
      |
      |     * refactor: Expand control flow on save/update
      |
      |     * refactor: Prevent unnecesary operations when deleting a new profile
      |
      | commit 5b69576068e47017a44c7f5b9268c1073ca43d1d
      | Author: William Woodruff <william@yossarian.net>
      | Date:   Mi 2021-03-24 18:33:30
      |
      |     clients: smash /usr/local/bin into the PATH on macOS (#49)
      |
      |     * clients: smash /usr/local/bin into the PATH on macOS
      |
      |     Fixes #48.

--- opt/ff2mpv.py
+++ opt/ff2mpv.py
@@ -2,9 +2,11 @@
 
 # Firefox "Open in MPV" plugin
 # forked and adapted from https://github.com/woodruffw/ff2mpv/blob/master/ff2mpv.py
+# commit b312d7e816a99f4c7abd (2024-12-03, William Woodruff: "Delete .github/FUNDING.yml")
 # for https://addons.mozilla.org/en-US/firefox/addon/ff2mpv/
 
 import json
+import os
 import platform
 import struct
 import sys
@@ -14,6 +16,7 @@ import subprocess
 def main():
     message = get_message()
     url = message.get("url")
+    options = message.get("options") or []
 
     args = ["mpve", url]
 
@@ -21,6 +24,17 @@ def main():
     # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#Closing_the_native_app
     if platform.system() == "Windows":
         kwargs["creationflags"] = subprocess.CREATE_BREAKAWAY_FROM_JOB
+
+    # HACK(ww): On macOS, graphical applications inherit their path from `launchd`
+    # rather than the default path list in `/etc/paths`. `launchd` doesn't include
+    # Homebrew in its default list, which means that any installations
+    # of MPV and/or youtube-dl under that prefix aren't visible when spawning
+    # from, say, Firefox. The real fix is to modify `launchd.conf`, but that's
+    # invasive and maybe not what users want in the general case.
+    # Hence this nasty hack.
+    if platform.system() == "Darwin":
+        path = os.environ.get("PATH")
+        os.environ["PATH"] = f"/opt/homebrew/bin:/usr/local/bin:{path}"
 
     subprocess.Popen(args, **kwargs)
 
@@ -40,7 +54,11 @@ def get_message():
 
 
 def send_message(message):
-    content = json.dumps(message).encode("utf-8")
+    # https://stackoverflow.com/a/56563264
+    # https://docs.python.org/3/library/json.html#basic-usage
+    # To get the most compact JSON representation, you should specify
+    # (',', ':') to eliminate whitespace.
+    content = json.dumps(message, separators=(",", ":")).encode("utf-8")
     length = struct.pack("@I", len(content))
     sys.stdout.buffer.write(length)
     sys.stdout.buffer.write(content)
