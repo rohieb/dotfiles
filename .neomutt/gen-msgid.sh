139db69b2f3073f7ecc6bc9412b03d81b939d371
Roland Hieber <rohieb@rohieb.name>
2025-03-25 12:14:46 +0100

    neomutt: generate better Message-ID headers
    Since a recent neomutt update, the Message-ID is only a random string of
    letters and numbers.

---
+++ .neomutt/gen-msgid.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# This generates friendly Message-ID headers that are safe, unique, and provide
+# better UX for someone using lore/b4 to retrieve messages.
+#
+# Instructions for using with mutt/neomutt:
+#
+# Save this as ~/.config/mutt/gen-msgid, then add
+# ~/.config/mutt/gen-msgid.muttrc with the following, fixing your path
+# to the file:
+#
+# my_hdr Message-ID: <`~/.config/mutt/gen-msgid.sh`>
+#
+# then edit ~/.config/mutt/muttrc to add:
+#
+# send-hook . "source ~/.config/mutt/gen-msgid.muttrc"
+
+# I like my msgid to start with the date
+msgid="$(date -u +%s)-"
+
+if [ -x "$(which coolname)" ]; then
+    msgid="${msgid}$(coolname 3)-$(openssl rand -hex 3)"
+elif [ -x "$(which diceware)" ]; then
+    # I like memorable nonsense, so I can visually tell one message
+    # from another, by looking at the lore URL, so use diceware for
+    # that
+    msgid="${msgid}$(diceware --no-caps -d- -n4)-$(openssl rand -hex 6)"
+else
+    # Just use openssl with some extra randomness
+    msgid="${msgid}$(openssl rand -hex 12)"
+fi
+
+msgid="${msgid}@$(hostname)"
+echo "${msgid}"
